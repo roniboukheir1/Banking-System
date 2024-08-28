d84e1b8905799e091a05adfb9fe7f574d27c0766
roniboukheir1 <roni.boukheir@lau.edu>
2024-08-28 16:11:19 +0300

    Added Docker and Docker compose files

--- BankingSystem/BankingSystem.Infrastructure/obj/Debug/net8.0/BankingSystem.Infrastructure.AssemblyInfo.cs
+++ BankingSystem/BankingSystem.Infrastructure/obj/Debug/net8.0/BankingSystem.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c3240c7ee97a1d1d01d63e8590fc29bc2fb935f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f66cbff732640170dd06c803ecf56e5771acba")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
